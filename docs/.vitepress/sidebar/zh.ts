0b478b44e79ffbbad9ad219cfef2c331113dbac9
许大仙 <1900919313@qq.com>
2025-02-11 10:40:39 +0800

    msg

---
+++ docs/.vitepress/sidebar/zh.ts
@@ -0,0 +1,106 @@
+import { DefaultTheme } from 'vitepress'
+import { commonDirectoryName } from '../utils/constant'
+
+// 中文侧边栏
+export const zhSidebar: DefaultTheme.Sidebar = {
+  '/': [
+    {
+      text: 'Java 语言基础',
+      collapsed: true,
+      items: [
+        { text: '计算机软件常识科普', link: `/01_java-basic/01_${commonDirectoryName}/` },
+        { text: 'Windows 软件包管理', link: `/01_java-basic/02_${commonDirectoryName}/` },
+        { text: 'Java 语言入门一', link: `/01_java-basic/03_${commonDirectoryName}/` },
+        { text: 'Java 语言入门二', link: `/01_java-basic/04_${commonDirectoryName}/` },
+        { text: 'Java 基础知识', link: `/01_java-basic/05_${commonDirectoryName}/` },
+        { text: '进制', link: `/01_java-basic/06_${commonDirectoryName}/` },
+        { text: 'Java 数据类型', link: `/01_java-basic/07_${commonDirectoryName}/` },
+        { text: 'Java 运算符', link: `/01_java-basic/08_${commonDirectoryName}/` },
+        { text: 'Java 开发工具（IDEA）', link: `/01_java-basic/09_${commonDirectoryName}/` },
+      ]
+    },
+    {
+      text: 'Java 语言进阶',
+      collapsed: true,
+      items: [
+        { text: '虚拟地址空间', link: `/02_java-leap/01_${commonDirectoryName}/` },
+      ]
+    },
+    {
+      text: 'Java 语言高级',
+      collapsed: true,
+      items: [
+
+      ]
+    },
+    {
+      text: '数据库核心技术',
+      collapsed: true,
+      items: [
+
+      ]
+    },
+    {
+      text: '项目构建工具',
+      collapsed: true,
+      items: [
+        {
+          text: 'Maven',
+          collapsed: true,
+          items: [
+            { text: 'Maven 核心概念', link: `/04_build/maven/01_${commonDirectoryName}/` },
+          ]
+        },
+        {
+          text: 'Gradle',
+          collapsed: true,
+          items: [
+            { text: '初识 Gradle', link: `/04_build/gradle/01_${commonDirectoryName}/` },
+          ]
+        },
+      ]
+    },
+    {
+      text: 'WEB 技术规范',
+      collapsed: true,
+      items: [
+
+      ]
+    },
+    {
+      text: '后端工程化',
+      collapsed: true,
+      items: [
+
+      ]
+    },
+    {
+      text: '前端工程化',
+      collapsed: true,
+      items: [
+
+      ]
+    },
+    {
+      text: '分布式核心',
+      collapsed: true,
+      items: [
+
+      ]
+    },
+    {
+      text: '微服务',
+      collapsed: true,
+      items: [
+
+      ]
+    },
+    {
+      text: '项目实战',
+      collapsed: true,
+      items: [
+
+      ]
+    }
+  ],
+}
---
+++ docs/.vitepress/theme/components/Meilisearch.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="VPNavBarSearch search">
+    <div id="docsearch"></div>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+
+import "meilisearch-docsearch/css";
+
+onMounted(async () => {
+  const meilisearchDetector = await import("meilisearch-docsearch");
+  meilisearchDetector.default({
+    container: "#docsearch",
+    host: "https://meilisearch.weiweixu.cn",
+    apiKey: "83add19fa3afb78d1c50078a477a36b873bfa354f85f38d28f939698f8e27c80",
+    indexUid: "java-weiweixu",
+    translations: {
+      button: {
+        buttonText: "搜索文档",
+        buttonAriaLabel: "搜索文档",
+      },
+      modal: {
+        title: "搜索文档",
+        placeholder: "搜索文档",
+        noResults: "没有找到结果",
+        showMore: "显示更多结果",
+        showLess: "显示更少结果",
+        emptyQuery: "请输入搜索词",
+        back: "返回",
+        searchDocsPlaceHolder: "搜索文档",
+        cancelButtonText: "取消",
+      },
+    },
+  });
+});
+</script>
+
+<style></style>
