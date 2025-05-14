afcaedb5abc36d4486934d7744523201fd3d4531
许大仙 <1900919313@qq.com>
2025-05-14 09:32:06 +0800

    docs(zh): 添加新的 SVG 图示到 Java 高级教程
    - 新增 `33.svg` 图示，用于完善 `java-senior` 部分内容展示
    - 文件遵循 SVG 1.1 标准，支持 draw.io 编辑

---
+++ docs/.vitepress/theme/components/LogoAnimate.vue
@@ -0,0 +1,21 @@
+<template></template>
+<script setup lang="ts">
+import {onMounted, ref} from "vue";
+import {gsap} from "gsap";
+
+function animate() {
+  const logo = ref('.VPImage.image-src')
+  gsap.from(logo.value, {
+    // x: -200,
+    x: '-50vw',
+    opacity: 0,
+    duration: 2,
+    ease: 'power3.out',
+    yoyo: true,
+  })
+}
+
+onMounted(() => {
+  animate();
+});
+</script>
--- docs/.vitepress/theme/components/TypeIt.vue
+++ docs/.vitepress/theme/components/TypeIt.vue
@@ -7,7 +7,6 @@ onMounted(() => {
   new TypeIt(".tagline", {
     strings: [
       "Java 是一种跨平台、面向对象的编程语言。",
-      "Java 具有高安全性和稳定性，广泛应用于企业级开发。",
     ],
     speed: 200,
   }).go();
