be9054c7a507b6ddc268974884b5399c22511da2
许大仙 <1900919313@qq.com>
2025-07-18 16:31:13 +0800

    docs(zh): 重构 JVM基础篇文档
    - 删除了旧的 JVM 基础篇文档
    - 新增了更详细、更深入的 JVM基础篇文档
    - 重新组织了文档结构，使其更加清晰
    - 增加了更多关于字节码文件、类加载器等内容的讲解

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -22,13 +22,40 @@ import 'virtual:group-icons.css'
 import './style/index.css'
 import xgplayer from "./components/Xgplayer.vue"
 
+// 彩虹背景动画样式
+function updateHomePageStyle(value: boolean) {
+  if (value) {
+    if (homePageStyle) return
+
+    homePageStyle = document.createElement('style')
+    homePageStyle.innerHTML = `
+    :root {
+      animation: rainbow 12s linear infinite;
+    }`
+    document.body.appendChild(homePageStyle)
+  } else {
+    if (!homePageStyle) return
+
+    homePageStyle.remove()
+    homePageStyle = undefined
+  }
+}
+
+let homePageStyle: HTMLStyleElement | undefined
 export default {
   extends: DefaultTheme,
   Layout() {
     return h(SwitchLayout)
   },
   enhanceApp({ app, router }: EnhanceAppContext) {
-
+    // 彩虹背景动画样式
+    if (typeof window !== 'undefined') {
+      watch(
+        () => router.route.data.relativePath,
+        () => updateHomePageStyle(location.pathname === '/'),
+        { immediate: true },
+      )
+    }
     // 开启详细的水合错误信息
     app.config.warnHandler = (msg, instance, trace) => {
       console.warn('[Vue warn]:', msg)
